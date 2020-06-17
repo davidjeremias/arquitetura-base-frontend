0639d9602d8f5ddb639b669173d03f0578aa4604
davidjeremias <com1.com3@gmail.com>
2020-06-17 19:49:47 -0300

    [Changed] estrutura basica de aplicações vue-js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Menu></Menu>
+    
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//import Header from '@/views/templates/Header.vue'
+import Menu from '@/views/templates/Menu.vue'
 export default {
   name: 'App',
+  props: {
+      source: String,
+    },
   components: {
-    HelloWorld
-  }
-}
+    //Header,
+    Menu
+  },
+  
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
